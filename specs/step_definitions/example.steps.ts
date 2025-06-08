1039394932470a6fb60ab1ea2170afe591efdc88
antoningar <antoningaranto@protonmail.com>
2025-06-09 01:05:00 +0200

    feat(oauth): update specs

--- specs/step_definitions/example.steps.ts
+++
@@ -1,9 +0,0 @@
-import { Then } from '@cucumber/cucumber';
-import { strict as assert } from 'assert';
-import { response } from './common.steps';
-
-Then('the response should contain the expected message', function () {
-  const body = JSON.parse(response.body);
-  assert.equal(body.message, 'Function executed successfully');
-  assert.ok(body.timestamp);
-});
